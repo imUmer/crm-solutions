d84be94582ae75427120f13fa7dabf8dfde96f68
imUmer <imumerniaz@gmail.com>
2025-02-22 00:24:10 +0300

    dashboard and sidebar | page 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,  // Marking as standalone
-  imports: [RouterOutlet],  // Importing RouterOutlet for routing
+  imports: [RouterLink,RouterOutlet, SidebarComponent],  // Importing RouterOutlet for routing
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,29 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 export const routes: Routes = [
     { path: '', component: MainLayoutComponent },  
     { path: 'signup', component: SignupComponent }, 
     { path: 'login', component: LoginComponent }, 
     { path: 'dashboard', component: DashboardComponent }, 
+    { path: '**', component: NotfoundComponent }, 
+     // Routes for authentication (without sidebar)
+     { path: '', component: MainLayoutComponent, children: [
+        { path: 'login', component: LoginComponent },
+        { path: 'signup', component: SignupComponent },
+    ]},
+
+    // Routes for dashboard and other main pages (with sidebar)
+    { path: '', component: MainLayoutComponent, children: [
+        { path: 'dashboard', component: DashboardComponent }, 
+        // Add more pages inside the dashboard layout if needed
+    ]},
+
+    // Redirect unknown paths to login
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
+
