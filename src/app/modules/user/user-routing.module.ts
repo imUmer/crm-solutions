9ffdfccbca9545702e9d8f14e66243c3b779f693
imUmer <imumerniaz@gmail.com>
2025-07-15 00:34:43 +0300

    fix routes

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: UserDashboardComponent, children: [
+      {path:"home", component: DashboardComponent},
+      {path:"", redirectTo:'/user/home', pathMatch: 'full'}
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
