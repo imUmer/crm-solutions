2c8d36177f8fbf6655d21493da4ad9a20790b0ca
imUmer <imumerniaz@gmail.com>
2025-07-24 23:20:43 +0300

    dialog box

---
+++ src/app/components/dialogbox/dialogbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogbox',
+  imports: [],
+  templateUrl: './dialogbox.component.html',
+  styleUrl: './dialogbox.component.scss'
+})
+export class DialogboxComponent {
+
+}
