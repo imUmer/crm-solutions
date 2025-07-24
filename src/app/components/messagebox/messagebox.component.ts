4738ef674f0716e452961f5056802a34c07c7528
imUmer <imumerniaz@gmail.com>
2025-07-24 23:21:03 +0300

    message box

---
+++ src/app/components/messagebox/messagebox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagebox',
+  imports: [],
+  templateUrl: './messagebox.component.html',
+  styleUrl: './messagebox.component.scss'
+})
+export class MessageboxComponent {
+
+}
