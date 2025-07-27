e018eb9edca85145f435a049a7b907029108e40f
imUmer <imumerniaz@gmail.com>
2025-07-27 23:35:29 +0300

    dialog box set up

--- src/app/components/dialogbox/dialogbox.component.ts
+++ src/app/components/dialogbox/dialogbox.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-dialogbox',
-  imports: [],
   templateUrl: './dialogbox.component.html',
-  styleUrl: './dialogbox.component.scss'
+  styleUrls: ['./dialogbox.component.scss']
 })
 export class DialogboxComponent {
+  @Input() title: string = 'Dialog Title';
+  @Input() message: string = 'Are you sure you want to continue?';
+  @Input() show: boolean = false;
 
+  @Output() close = new EventEmitter<boolean>();
+
+  onClose(result: boolean) {
+    this.close.emit(result);
+    this.show = false;
+  }
 }
